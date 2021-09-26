cfda93bf5ba7463b0b1e01d33099e36520cd68d8
dydwn <dydwn3243@naver.com>
2021-09-26 19:19:43 +0900

    java

--- src/main/java/com/pracProject/springboot/domain/posts/Posts.java
+++ src/main/java/com/pracProject/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.pracProject.springboot.domain.posts;
 
+import com.pracProject.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +13,7 @@ import javax.persistence.Id;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id
     @GeneratedValue
     public Long id;
