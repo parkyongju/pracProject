69b93f212aa1a21238de4aa409840eeb26b99146
dydwn <dydwn3243@naver.com>
2021-09-28 23:28:00 +0900

    java

--- src/main/java/com/pracProject/springboot/service/posts/PostsService.java
+++ src/main/java/com/pracProject/springboot/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.pracProject.springboot.service.posts;
 
 import com.pracProject.springboot.domain.posts.Posts;
 import com.pracProject.springboot.domain.posts.PostsRepository;
+import com.pracProject.springboot.web.dto.PostsListResponseDto;
 import com.pracProject.springboot.web.dto.PostsResponseDto;
 import com.pracProject.springboot.web.dto.PostsSaveRequestDto;
 import com.pracProject.springboot.web.dto.PostsUpdateRequestDto;
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.transaction.Transactional;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +37,10 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 업습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional //(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 }
